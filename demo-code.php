0f57bcb2141972ed8922e2a1b850db6f2a2bdd58
Eleas Kanchon <kanchonruet@gmail.com>
2020-07-23 21:49:05 +0600

    ff

--- demo-code.php
+++ demo-code.php
@@ -1,7 +1,8 @@
               <!--/.include this where you want to show add to cart button-->
 
               <?php
-							global $product;
-							$pid = $product->get_id();
-							?>
-							<a href="<?php echo do_shortcode( '[add_to_cart_url id=' . $pid . ']' ) ?>" class="your-classes-here">Add to cart this</a>
+                global $product;
+                $pid = $product->get_id();
+	        ?>
+	<a href="<?php echo do_shortcode( '[add_to_cart_url id=' . $pid . ']' ) ?>" class="your-classes-here">Add to cart this</a>
+							
